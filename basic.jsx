8d90a8c22b82dc528acdb3cf772f5d9fe113affd
poove <poovendranselavaraj333@gmail.com>
2024-08-10 18:02:51 +0530

    chapter 1

--- basic.jsx
+++ basic.jsx
@@ -1,3 +1,47 @@
 React is open source js library
 used for build user interfaces(UI)
 create single page applications by using reusable components
+
+"7 keys" of features of React
+.virtual DOM
+.component based architecture
+.reusability & Composition
+.jsx (javascript XML)
+.declarative Syntax
+.community & ecosystem
+.react hooks
+
+DOM-Document Object Model
+.tree-like structure
+HTML and DOM are same but HTML denote as a tags and DOM denote as tree structure and also machine language.
+
+// virtual DOM #doubt
+
+React components is reusable building block for user interface 
+
+single page applications is a web applications that loads only one page at a time.
+
+5 Advantages of React
+.simple to build single page applications(by using components)
+.react is cross platform and open source(free to use)
+.Lightweight and very FontFaceSet(virtual DOM)
+.Large community and ecosystem
+.Testing the UI is easy
+
+
+disadvantages of React
+.react is not a good choice for very vry small applications
+
+role of JSX in react:
+.JSX stands for JavaScript XML
+.JSX is used by React to write HTML-like code.
+.JSX is converted into JavaScript by Babel.
+
+Declarative syntax using JSX
+its not involves step by step Process 
+JSX in React is used to write declarative syntax.
+
+Imperative syntax (non-react) using JavaScript
+its involves step by step Process to achieve a particular goal.
+javascript has an Imperative syntax.
+
