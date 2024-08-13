4ab5e7d8988e18b8e91e07b1c09a1374a4748519
poove <poovendranselavaraj333@gmail.com>
2024-08-13 13:08:07 +0530

    1.05.00

--- basic 2.jsx
+++ basic 2.jsx
@@ -1,2 +1,7 @@
 Arrow function Expression #doubt
 
+React & Angular both are used to create single page UI app using customElements.
+
+state,stateless,statefull #doubt 
+
+JSX is convert into JS with a help of Babel.bcoz browser not understand JSX only JS.
